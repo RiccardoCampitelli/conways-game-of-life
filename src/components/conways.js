9153b0a15243dc486a1a63915587b0ff94113100
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2020-02-04 20:55:24 +0000

    refactor conways simulator to component

--- src/components/conways.js
+++ src/components/conways.js
@@ -1,31 +1,11 @@
-import React, { useState, useCallback, useRef } from "react";
+import React from "react";
+import Simulator from "./simulator";
 
 import styled from "styled-components";
 
-import Grid from "./grid";
 import GameDescription from "#root/components/gameDescription";
 
-import produce from "immer";
-import useInterval from "#root/hooks/useInterval";
-
-import {
-  Slider,
-  Section,
-  Button,
-  Row,
-  StyledDiv
-} from "#root/components/common/styledComponents";
-import useDimensions from "../hooks/useDimensions";
-
-import { useWorker } from "react-hooks-worker";
-
-const Container = styled.div`
-  background-color: #f5f5f5;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+import useDimensions from "#root/hooks/useDimensions";
 
 const Header = styled.h1`
   margin: 0;
@@ -45,112 +25,24 @@ const Header = styled.h1`
   z-index: 5;
 `;
 
-const BOARD_HEIGHT = 50;
-const BOARD_WIDTH = 50;
-
-const LIFE_RATIO = 0.2;
-
-const TICK_SPEED = 850;
-const MIN_TICK_SPEED = 250;
-const MAX_TICK_SPEED = 1450;
-
-const generateEmptyGrid = () =>
-  Array.from({ length: BOARD_WIDTH }, () =>
-    Array.from({ length: BOARD_HEIGHT }, () => 0)
-  );
-
-const generateRandomGrid = () =>
-  Array.from({ length: BOARD_WIDTH }, () =>
-    Array.from({ length: BOARD_HEIGHT }, () =>
-      Math.random() < LIFE_RATIO ? 1 : 0
-    )
-  );
-
-const createWorker = () => new Worker("../worker/conwayWorker.js");
-
-//TODO: try https://github.com/react-component/slider for slider
-//TODO: add controls wrapper / container
-//TODO: Move common components out of here.
+const Container = styled.div`
+  background-color: #f5f5f5;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Conways = () => {
-  const [grid, setGrid] = useState(generateRandomGrid());
-  const [tickSpeed, setTickSpeed] = useState(TICK_SPEED);
-  const [running, setRunning] = useState(false);
-
-  const { result } = useWorker(createWorker, grid);
   const [ref, { width }] = useDimensions();
-
-  const gridRef = useRef();
-  gridRef.current = result;
-
-  const setNewGrid = useCallback(() => {
-    if (gridRef.current !== undefined) {
-      setGrid(gridRef.current.grid);
-    }
-  }, []);
-
   const isSmallScreen = Math.floor(width) < 750;
-
-  useInterval(setNewGrid, running ? tickSpeed : null);
-
-  const toggleRunning = () => {
-    setRunning(running => !running);
-  };
-
-  const clearGrid = () => {
-    setRunning(false);
-    setGrid(generateEmptyGrid());
-  };
-
-  const randomiseGrid = () => {
-    setGrid(generateRandomGrid());
-  };
-
-  const handleSpeedChange = useCallback(event => {
-    event.persist();
-    setTickSpeed(event.target.value);
-  }, []);
-
-  const flipCell = useCallback((rowIndex, colIndex) => {
-    setGrid(oldGrid => {
-      return produce(oldGrid, gridCopy => {
-        gridCopy[rowIndex][colIndex] = gridCopy[rowIndex][colIndex] ? 0 : 1;
-      });
-    });
-  }, []);
-
-  const speedPercentage = Math.floor(
-    (1 -
-    (tickSpeed - MIN_TICK_SPEED) / (MAX_TICK_SPEED - MIN_TICK_SPEED)) * 100
-  )
 
   return (
     <Container ref={ref}>
       <Header>Conways game of life</Header>
 
       <GameDescription />
-
-      <Row pt={15} pb={15} isSmallScreen={isSmallScreen}>
-        <Section>
-          <Button onClick={toggleRunning}>{running ? "Stop" : "Start"}</Button>
-          <Button onClick={randomiseGrid}>Randomise</Button>
-          <Button onClick={clearGrid}>Clear</Button>
-        </Section>
-        <Section>
-          <StyledDiv>Speed {speedPercentage}%</StyledDiv>
-          <Slider
-            value={tickSpeed}
-            type="range"
-            step="50"
-            min={MIN_TICK_SPEED}
-            max={MAX_TICK_SPEED}
-            onChange={handleSpeedChange}
-          />
-        </Section>
-      </Row>
-      <Row pt={20} pb={20}>
-        <Grid grid={grid} flipCell={flipCell} />
-      </Row>
+      <Simulator isSmallScreen={isSmallScreen} />
     </Container>
   );
 };
--- src/components/simulator.js
+++ src/components/simulator.js
@@ -1,16 +1,150 @@
+import React, { useState, useCallback, useRef } from "react";
 
-import React from "react";
+import styled from "styled-components";
 
+import Grid from "./grid";
+import GameDescription from "#root/components/gameDescription";
 
-const Conways = () => {
+import produce from "immer";
+import useInterval from "#root/hooks/useInterval";
 
+import {
+  Slider,
+  Section,
+  Button,
+  Row,
+  StyledDiv
+} from "#root/components/common/styledComponents";
+import useDimensions from "../hooks/useDimensions";
 
+import { useWorker } from "react-hooks-worker";
 
-    return (
-       <div>
-         Hey
-       </div>
+const Container = styled.div`
+  background-color: #f5f5f5;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.h1`
+  margin: 0;
+
+  background-color: #c66;
+  border-radius: 15px;
+  width: 80%;
+  height: 100px;
+  line-height: 80px;
+  font-size: 60px;
+  text-align: center;
+  position: relative;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-family: "Pacifico", cursive;
+  color: #f5f5f5;
+  z-index: 5;
+`;
+
+const BOARD_HEIGHT = 50;
+const BOARD_WIDTH = 50;
+
+const LIFE_RATIO = 0.2;
+
+const TICK_SPEED = 850;
+const MIN_TICK_SPEED = 250;
+const MAX_TICK_SPEED = 1450;
+
+const generateEmptyGrid = () =>
+  Array.from({ length: BOARD_WIDTH }, () =>
+    Array.from({ length: BOARD_HEIGHT }, () => 0)
+  );
+
+const generateRandomGrid = () =>
+  Array.from({ length: BOARD_WIDTH }, () =>
+    Array.from({ length: BOARD_HEIGHT }, () =>
+      Math.random() < LIFE_RATIO ? 1 : 0
     )
-}
+  );
 
-export default Conways;
+const createWorker = () => new Worker("../worker/conwayWorker.js");
+
+//TODO: try https://github.com/react-component/slider for slider
+//TODO: add controls wrapper / container
+//TODO: Move common components out of here.
+
+const Simulator = ({ isSmallScreen }) => {
+  const [grid, setGrid] = useState(generateRandomGrid());
+  const [tickSpeed, setTickSpeed] = useState(TICK_SPEED);
+  const [running, setRunning] = useState(false);
+
+  const { result } = useWorker(createWorker, grid);
+
+  const gridRef = useRef();
+  gridRef.current = result;
+
+  const setNewGrid = useCallback(() => {
+    if (gridRef.current !== undefined) {
+      setGrid(gridRef.current.grid);
+    }
+  }, []);
+
+  useInterval(setNewGrid, running ? tickSpeed : null);
+
+  const toggleRunning = () => {
+    setRunning(running => !running);
+  };
+
+  const clearGrid = () => {
+    setRunning(false);
+    setGrid(generateEmptyGrid());
+  };
+
+  const randomiseGrid = () => {
+    setGrid(generateRandomGrid());
+  };
+
+  const handleSpeedChange = useCallback(event => {
+    event.persist();
+    setTickSpeed(event.target.value);
+  }, []);
+
+  const flipCell = useCallback((rowIndex, colIndex) => {
+    setGrid(oldGrid => {
+      return produce(oldGrid, gridCopy => {
+        gridCopy[rowIndex][colIndex] = gridCopy[rowIndex][colIndex] ? 0 : 1;
+      });
+    });
+  }, []);
+
+  const speedPercentage = Math.floor(
+    (1 - (tickSpeed - MIN_TICK_SPEED) / (MAX_TICK_SPEED - MIN_TICK_SPEED)) * 100
+  );
+
+  return (
+    <>
+      <Row pt={15} pb={15} isSmallScreen={isSmallScreen}>
+        <Section>
+          <Button onClick={toggleRunning}>{running ? "Stop" : "Start"}</Button>
+          <Button onClick={randomiseGrid}>Randomise</Button>
+          <Button onClick={clearGrid}>Clear</Button>
+        </Section>
+        <Section>
+          <StyledDiv>Speed {speedPercentage}%</StyledDiv>
+          <Slider
+            value={tickSpeed}
+            type="range"
+            step="50"
+            min={MIN_TICK_SPEED}
+            max={MAX_TICK_SPEED}
+            onChange={handleSpeedChange}
+          />
+        </Section>
+      </Row>
+      <Row pt={20} pb={20}>
+        <Grid grid={grid} flipCell={flipCell} />
+      </Row>
+    </>
+  );
+};
+
+export default Simulator;
